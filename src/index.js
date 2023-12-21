48da40622960d45b8f4fb816cf73c980b826edea
Shivanshika Agarwal <113771299+ShivanshikaAgarwal1505@users.noreply.github.com>
2023-12-21 18:20:11 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React, { Component, useState, useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
+import MultiStepForm from './Components/form';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <MultiStepForm/>
+  </React.StrictMode>
+);
